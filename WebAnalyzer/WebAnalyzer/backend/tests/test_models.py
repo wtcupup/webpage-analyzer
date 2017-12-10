8bcf371b008dfe01c5fc68f86100d9e710529f70
Natalie <natalie.m.giordana@vanderbilt.edu>
2017-12-10 17:39:11 -0600

    fix test_models and force 2 tests to fail

--- WebAnalyzer/WebAnalyzer/backend/tests/test_models.py
+++ WebAnalyzer/WebAnalyzer/backend/tests/test_models.py
@@ -9,27 +9,24 @@ The website refers to this repo:
 https://github.com/mjhea0/testing-in-django
 """
 
-
 # models tests
+
 class WebsiteListTest(TestCase):
     def create_websitelist(self, name= "someName"):
         return WebsiteList.objects.create(name=name)
-
     # test if name is as expected
     def test_websitelist_creation(self):
         w=self.create_websitelist()
         self.assertTrue(isinstance(w, WebsiteList))
-        self.assertEqual(w.__unicode__(), w.name)
+        self.assertEqual(w.__unicode__(), "anotherName")
 
 
 class WebsiteTest(TestCase):
-
-	def create_website(self, list= "fill in", url= "http://www.disney.com", was_searched=True,count=0):
-		return Website.objects.create(list=list, url=url, was_searched=was_searched, count=count)
-
-    # test if url is as expected
+    def create_website(self, list=WebsiteList.objects.create(name="someName"), url= "http://www.disney.com", was_searched=True,count=0):
+        return Website.objects.create(list=list, url=url, was_searched=was_searched, count=count)
+    #check if url is correct
     def test_website_creation(self):
-         w = self.create_website()
-         self.assertTrue(isinstance(w, Website))
-         self.assertEqual(w.__unicode__(), w.url) #do we need to use reverse for this url?
+        w = self.create_website()
+        self.assertTrue(isinstance(w, Website))
+        self.assertEqual(w.__unicode__(), "http://nickelodeon.com")  # do we need to use reverse for this url?
 
